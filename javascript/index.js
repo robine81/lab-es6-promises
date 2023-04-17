d6aa48b431949714a030a59fc66f2f97d9cc1b85
RuiMSMelo <ruimsmelo@gmail.com>
2023-04-17 14:02:29 +0100

    all done

--- javascript/index.js
+++ javascript/index.js
@@ -39,10 +39,13 @@ getInstruction("mashedPotatoes", 0, (step1) => {
           getInstruction("mashedPotatoes", 4, (step5) => {
               document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
               document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
+              const mashedPotatoesImage = document.querySelector('#mashedPotatoesImg')
+              mashedPotatoesImage.style.display = "block"
             }, (error) => console.log(error));
            }, (error) => console.log(error));          
          }, (error) => console.log(error));
     }, (error) => console.log(error));
+
   }
 );
 
@@ -69,11 +72,68 @@ obtainInstruction('steak', 0)
   }).then( (step6) => {
     document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
     document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+    const steakImage = document.querySelector('#steakImg')
+    steakImage.style.display = "block"
     return obtainInstruction('steak', 7)
   });
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  console.log("asd")
+  try {
+    const valueFromPreviousPromise = await obtainInstruction('broccoli', 0)
+    document.querySelector("#broccoli").innerHTML += `<li>${valueFromPreviousPromise}</li>`
+
+    const valueFromPreviousPromiseTwo = await obtainInstruction('broccoli', 1)
+    document.querySelector("#broccoli").innerHTML += `<li>${valueFromPreviousPromiseTwo}</li>`
+
+    const valueFromPreviousPromiseThree = await obtainInstruction('broccoli', 2)
+    document.querySelector("#broccoli").innerHTML += `<li>${valueFromPreviousPromiseThree}</li>`
+
+    const valueFromPreviousPromiseFour = await obtainInstruction('broccoli', 3)
+    document.querySelector("#broccoli").innerHTML += `<li>${valueFromPreviousPromiseFour}</li>`
+
+    const valueFromPreviousPromiseFive = await obtainInstruction('broccoli', 4)
+    document.querySelector("#broccoli").innerHTML += `<li>${valueFromPreviousPromiseFive}</li>`
+
+    const valueFromPreviousPromiseSix = await obtainInstruction('broccoli', 5)
+    document.querySelector("#broccoli").innerHTML += `<li>${valueFromPreviousPromiseSix}</li>`
+
+    const valueFromPreviousPromiseSeven = await obtainInstruction('broccoli', 6)
+    document.querySelector("#broccoli").innerHTML += `<li>${valueFromPreviousPromiseSeven}</li>`
+  }
+  catch (error) {
+    console.log("Entering error statement")
+    console.error(error);
+  }
+  
+  document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+  const broccoliImage = document.querySelector('#broccoliImg')
+  broccoliImage.style.display = "block"
+}
+makeBroccoli()
 
 // Bonus 2 - Promise all
-// ...
+async function makeBrusselSprouts() {
+  console.log("check")
+  let brusselsSproutsArr = []
+  brusselsSprouts.forEach ((currentSprout, currentIndex) => {
+    brusselsSproutsArr.push(obtainInstruction('brusselsSprouts' ,currentIndex))
+  })
+  const pizza = await Promise.all(brusselsSproutsArr)
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[0]}</li>`
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[1]}</li>`
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[2]}</li>`
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[3]}</li>`
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[4]}</li>`
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[5]}</li>`
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[6]}</li>`
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${pizza[7]}</li>`
+
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels sprouts are ready!</li>`;
+  const brusselsSproutsImage = document.querySelector('#brusselsSproutsImg')
+  brusselsSproutsImage.style.display = "block"
+
+  console.log(pizza)
+}
+makeBrusselSprouts()
